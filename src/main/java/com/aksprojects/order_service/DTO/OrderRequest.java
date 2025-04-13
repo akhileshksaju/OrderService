f99d2382b63a27a1764d44192610955c43670a1b
akhileshksaju <akhilesh.saju1997@gmail.com>
2025-04-13 15:10:26 -0400

    OrderRequest change add Builder annotaition

--- src/main/java/com/aksprojects/order_service/DTO/OrderRequest.java
+++ src/main/java/com/aksprojects/order_service/DTO/OrderRequest.java
@@ -1,5 +1,16 @@
 package com.aksprojects.order_service.DTO;
 
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class OrderRequest {
+
+  private List<OrderLineItemDTO> orderLineItems;
 
 }
